29f6cba4df76d0621c0a8028c6d20891b905edb3
Oyvind Moller Asbjornsen <oyvindma@gmail.com>
2014-05-27 22:28:50 +0200

    Added command and handler for creating courses

---
+++ src/test/java/applicationservices/course/CreateCourseCommandHandlerTest.java
@@ -0,0 +1,41 @@
+package applicationservices.course;
+
+import integrationservices.InMemoryCourseRepository;
+
+import java.util.UUID;
+
+import org.fest.assertions.Assertions;
+import org.junit.Test;
+
+public class CreateCourseCommandHandlerTest {
+
+	@Test
+	public void shouldNotThrowExceptionWhenHandlingCreateCourseCommand() {
+		// Given
+		CreateCourseCommand createCourseCommand = new CreateCourseCommand(
+				UUID.randomUUID(), "course name");
+		CreateCourseCommandHandler createCourseCommandHandler = new CreateCourseCommandHandler(
+				new InMemoryCourseRepository());
+
+		// When
+		createCourseCommandHandler.handle(createCourseCommand);
+	}
+
+	@Test
+	public void shouldPersistCourse() {
+		// Given
+		InMemoryCourseRepository courseRepository = new InMemoryCourseRepository();
+		CreateCourseCommandHandler commandHandler = new CreateCourseCommandHandler(
+				courseRepository);
+
+		// When
+		UUID courseId = UUID.randomUUID();
+		CreateCourseCommand createCourseCommand = new CreateCourseCommand(
+				courseId, "course name");
+		commandHandler.handle(createCourseCommand);
+
+		// Then
+		Assertions.assertThat(courseRepository.get(courseId)).isNotNull();
+	}
+
+}
